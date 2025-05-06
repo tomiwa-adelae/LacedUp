93155e384507e68243c907722ef4df41a12cacc0
Tomiwa Adelae <thetommedia@gmail.com>
2025-05-06 19:08:52 +0100

    Fixed an error

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -11,6 +11,7 @@ const badgeVariants = cva(
 			variant: {
 				default:
 					"border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+				black: "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
 				secondary:
 					"border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
 				success:
